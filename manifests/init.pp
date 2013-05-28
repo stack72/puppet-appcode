92ec1be6402bc475f83ea98f8fef986e12fc272d
stack72 <public@paulstack.co.uk>
2013-05-28 17:04:36 +0100

    fixing the manifest file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,5 @@
-class webstorm ($version = '2.0.3') {
+class appcode {
+  $version = '2.0.3'
   package { 'AppCode':
     provider => 'appdmg',
     source   => "http://download.jetbrains.com/objc/AppCode-${version}.dmg",
